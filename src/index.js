b84cfe9377cab8252dbe6a7c0712b3a0755e8325
CongregacionOJLS <congregacion.ojls@gmail.com>
2024-07-30 16:13:20 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+/*root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+); */
+
+ReactDOM.render(
+  <Router>
+    <App></App>
+  </Router>
+  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
