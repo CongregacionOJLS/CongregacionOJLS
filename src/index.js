98679ab72ed65d419326cd40aeccf99887c5ec52
CongregacionOJLS <congregacion.ojls@gmail.com>
2024-07-31 16:38:39 -0300

    nineteenth commit

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 /*root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );*/
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <App />
