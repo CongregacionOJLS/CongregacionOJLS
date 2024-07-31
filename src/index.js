0d4f22ac2aa7434190aa77d112de12d537d75230
CongregacionOJLS <congregacion.ojls@gmail.com>
2024-07-31 16:52:11 -0300

    twenty two commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 /*root.render(
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
   </React.StrictMode>
 );*/
 root.render(
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
