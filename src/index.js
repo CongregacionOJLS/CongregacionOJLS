e0d3249e94e6cf6dc226531ea2e669f9f256ff68
CongregacionOJLS <congregacion.ojls@gmail.com>
2024-07-31 16:12:39 -0300

    fourteenth commit

--- src/index.js
+++ src/index.js
@@ -4,21 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter as Router } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-/*root.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-); */
-
-ReactDOM.render(
-  <Router>
-    <App></App>
-  </Router>
-  , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
